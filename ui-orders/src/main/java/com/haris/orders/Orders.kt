c4d3743834c717f92441fbcb5ea5de4eb82db27b
kurbardovikjharis <kurbardovikjharis@gmail.com>
2024-02-25 00:18:39 +0100

    init orders

--- ui-orders/src/main/java/com/haris/orders/Orders.kt
+++ ui-orders/src/main/java/com/haris/orders/Orders.kt
@@ -1,9 +1,76 @@
 package com.haris.orders
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.haris.resources.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Orders() {
-    Text(text = "orders")
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.orders),
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+            )
+        }
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(horizontal = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.no_orders_yet),
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Text(
+                    text = stringResource(id = R.string.no_orders_yet_description),
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {}
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.find_pizzeria),
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = stringResource(id = R.string.what_are_you_waiting_for),
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+        }
+    }
 }
