8ebb1ee0d235f59531597174b9a00305c88e1cd6
kurbardovikjharis <kurbardovikjharis@gmail.com>
2024-02-24 22:16:56 +0100

    Update RestaurantDetails.kt

--- ui-restaurantdetails/src/main/java/com/haris/restaurantdetails/RestaurantDetails.kt
+++ ui-restaurantdetails/src/main/java/com/haris/restaurantdetails/RestaurantDetails.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -23,10 +24,12 @@ fun RestaurantDetails(navigateUp: () -> Unit) {
                     Text(text = stringResource(id = R.string.restaurant_details))
                 },
                 navigationIcon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_arrow_back_24),
-                        contentDescription = stringResource(id = R.string.back)
-                    )
+                    IconButton(onClick = navigateUp) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_arrow_back_24),
+                            contentDescription = stringResource(id = R.string.back)
+                        )
+                    }
                 }
             )
         }
